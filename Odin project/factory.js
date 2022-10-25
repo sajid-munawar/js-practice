e25db6077472e5ac5d13057d215f6991c02c0579
sajid-munawar <sajidmunawar08@gmail.com>
2022-10-25 11:19:59 +0100

    Add Factory function

---
+++ Odin project/factory.js
@@ -0,0 +1,43 @@
+const Player = (name, level) => {
+    let health = level * 2;
+    const getLevel = () => level;
+    const getName = () => name;
+    const die = () => {
+        // uh oh
+    };
+    const damage = x => {
+        health -= x;
+        if (health <= 0) {
+            die();
+        }
+    };
+    const attack = enemy => {
+        if (level < enemy.getLevel()) {
+            damage(1);
+            console.log(`${enemy.getName()} has damaged ${name}`);
+        }
+        if (level >= enemy.getLevel()) {
+            enemy.damage(1);
+            console.log(`${name} has damaged ${enemy.getName()}`);
+        }
+    };
+    return { attack, damage, getLevel, getName };
+};
+const jimie = Player('jim', 10);
+const badGuy = Player('bad', 4);
+jimie.attack(badGuy);
+
+
+const Person = (name) => {
+    const sayName = () => console.log(`my name is${name}`);
+    return { sayName };
+}
+const Nerd = (name) => {
+    const { sayName } = Person(name);
+    const doSomethingNerdy = () => console.log('Nerd stuff');
+    return { sayName, doSomethingNerdy };
+}
+
+const jeff = Nerd('jeff');
+jeff.sayName();
+jeff.doSomethingNerdy();
